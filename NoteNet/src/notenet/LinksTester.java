62fcaaa2772be969aca67d91dc6350e1a82f4c02
Edwin Monk-Fromont <edwin.monkfromont@gmail.com>
2013-01-23 10:10:16 +1300

    links testing

--- NoteNet/src/notenet/LinksTester.java
+++ NoteNet/src/notenet/LinksTester.java
@@ -6,9 +6,9 @@ public class LinksTester {
 	
 	public static void main(String[] args){
 		LinkStore store = new LinkStore();
-		store.addNote("1");
-		store.addNote("2");
-		store.addNote("3");
+		store.addNote("1", "Test one");
+		store.addNote("2", "Hello");
+		store.addNote("3", "Blah");
 		store.setLink("1", "3", 0.5);	
 		for(Entry<String,Double> e : store.getLinks("1").entrySet()){
 			System.out.println(e.getKey() + " with weight " + e.getValue());
